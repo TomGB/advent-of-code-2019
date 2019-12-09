ddf2cf4392b9caefaf72447df9a0bed772d07abc
Thomas Banister <tgbanister@gmail.com>
2019-12-09 06:24:43 +0000

    wip on 7

--- 7b.machine.js
+++ 7b.machine.js
@@ -4,26 +4,25 @@ const machine = (inputData, input, pointer = 0) => {
     const twoParams = ({ param1, param2, paramMode, instructions }) => {
         const inputA = paramMode[0] ? param1 : instructions[param1]
         const inputB = paramMode[1] ? param2 : instructions[param2]
-    
+
         return { inputA, inputB }
     }
-    
+
     const commands = {
         1: ({ param1, param2, param3, paramMode, instructions, pointer }) => {
             const { inputA, inputB } = twoParams({ param1, param2, param3, paramMode, instructions })
-    
+
             instructions[param3] = inputA + inputB
             return pointer + 4
         },
         2: ({ param1, param2, param3, paramMode, instructions, pointer }) => {
             const { inputA, inputB } = twoParams({ param1, param2, paramMode, instructions })
-    
+
             instructions[param3] = inputA * inputB
             return pointer + 4
         },
         3: ({ param1, instructions, pointer }) => {
             instructions[param1] = parseInt(input[inputIndex])
-            console.log(instructions[param1])
             inputIndex++
             return pointer + 2
         },
@@ -49,33 +48,32 @@ const machine = (inputData, input, pointer = 0) => {
             return pointer + 4
         }
     }
-    
+
     const getParameterMode = (command) => {
         const [paramMode3, paramMode2, paramMode1, opCodePart1, opCodePart2] = command.toString().padStart(5,'0').split('')
         const opCode = opCodePart1 + opCodePart2
-    
+
         return {
             paramMode: [parseInt(paramMode1), parseInt(paramMode2), parseInt(paramMode3)],
             opCode: parseInt(opCode)
         }
     }
-    
+
     const program = (instructions) => {
         let outcome
         do {
             [command, param1, param2, param3] = instructions.slice(pointer, pointer + 4)
-    
+
             if (command == 99) return 'end'
 
             const {
                 paramMode,
                 opCode
             } = getParameterMode(command)
-    
+
             outcome = commands[parseInt(opCode)]({ param1, param2, param3, instructions, paramMode, pointer })
 
             if (typeof outcome !== 'number') {
-                console.log('program output')
                 return outcome
             }
 
@@ -83,8 +81,12 @@ const machine = (inputData, input, pointer = 0) => {
 
         } while (pointer < instructions.length)
     }
-    
-    return program(inputData.slice())
+
+    try {
+        return program(inputData.slice())
+    } catch (e) {
+        return {}
+    }
 }
 
 module.exports = machine
